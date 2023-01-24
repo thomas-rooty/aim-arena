c11fa6bd9afd85fed6880bf8fc985fa6258e66cb
thomas-rooty <tc.caron91@gmail.com>
2023-01-24 14:42:27 +0100

    made fps view

--- app/scene.tsx
+++ app/scene.tsx
@@ -1,11 +1,16 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
+import Controls from "@/utils/Controls";
 
 const Scene = () => {
   return (
-    <Canvas>
+    <Canvas
+      camera={{ position: [-1, 1, 5], fov: 65 }}
+    >
+      <color attach="background" args={["lightblue"]} />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
+      <Controls />
       <mesh>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="hotpink" />
